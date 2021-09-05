0687d98f2c335852a03f60f3cb547bc1a1b46438
vandy ahmad <vandyahmad2404@gmail.com>
2021-09-06 02:11:08 +0700

    contentx timeout

--- context_test.go
+++ context_test.go
@@ -3,7 +3,9 @@ package Context
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -36,4 +38,75 @@ func TestWithValue(t *testing.T) {
 	fmt.Println(contextF.Value("c"))
 	fmt.Println(contextF.Value("b"))
 	fmt.Println(contextA.Value("b"))
+}
+
+func CreateCounter(ctx context.Context) chan int{
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+		counter :=1
+		for  {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+
+
+func CreateCounterWithDelay(ctx context.Context) chan int{
+	destination := make(chan int)
+	go func() {
+		defer close(destination)
+		counter :=1
+		for  {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+				time.Sleep(1*time.Second) //simulasiuntuk delay
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total goroutine", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+	destination := CreateCounter(ctx)
+	for n := range destination{
+		fmt.Println("Counter",n)
+		if n==10{
+			break
+		}
+	}
+	cancel()
+	time.Sleep(2*time.Second)
+	fmt.Println("Total goroutine", runtime.NumGoroutine())
+}
+
+func TestContextWithTimeOut(t *testing.T) {
+	fmt.Println("Total goroutine", runtime.NumGoroutine())
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent,5*time.Second)
+	defer cancel()
+	destination := CreateCounterWithDelay(ctx)
+	for n := range destination{
+		fmt.Println("Counter",n)
+	}
+	//cancel()
+	time.Sleep(2*time.Second)
+	fmt.Println("Total goroutine", runtime.NumGoroutine())
 }
